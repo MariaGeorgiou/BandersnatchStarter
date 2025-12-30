bfae8759fda17f4c2461fe525e51f5943492e36f
MariaMcC <mariamccluskey1@gmail.com>
2025-12-30 07:46:32 -0800

    Sprint 2: add Altair visualization

--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,30 @@
-from altair import Chart
+from altair import Chart, Tooltip
+from pandas import DataFrame
 
+def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    properties = {
+        "width": 600,
+        "height": 400,
+        "background": "#2a303c",
+        "padding": 20,
+    }
 
-def chart(df, x, y, target) -> Chart:
-    pass
+    graph = (
+        Chart(df, title=f"{y} by {x} for {target}")
+        .mark_circle(size=100)
+        .encode(
+            x=f"{x}:Q",
+            y=f"{y}:Q",
+            color=f"{target}:N",
+            tooltip=Tooltip(df.columns.to_list())
+        )
+        .properties(**properties)
+        .configure(
+            axis={"labelColor": "#ffffff", "titleColor": "#ffffff"},
+            title={"color": "#ffffff"},
+            view={"stroke": "transparent"},
+            background="#2a303c",
+        )
+    )
+
+    return graph
